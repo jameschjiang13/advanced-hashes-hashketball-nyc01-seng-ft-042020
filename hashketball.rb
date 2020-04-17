51570cfa5725fd02620dc8fe0360fd011d51c30b
James Jiang <monochrome473@gmail.com>
2020-04-17 00:34:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -224,7 +224,6 @@ def big_shoe_rebounds
 
   counter_2 = 0
   while counter_2 < 5 do
-    binding.pry
     if hashketball[:home][:players][counter_2][:shoe] = biggest_shoe_size
       return hashketball[:home][:players][counter_2][:rebounds]
     end
